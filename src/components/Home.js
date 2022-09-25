93d702e64f2ab3c5916ff8dc3026c1862307a728
AdityaArnav <99977930+AdityaArnav@users.noreply.github.com>
2022-09-25 16:50:42 +0530

    added some changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useState} from "react"
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Sign_img from './Sign_img';
 
 const Home = () => {
+    const [inpval,setInpval] = useState({
+        name:'',
+        email:'',
+        data:'',
+        password:''
+    })
+    console.log(inpval)
+    const getData= (e)=>{
+        // console.log(e.target.value);
+        const {name,value} = e.target;
+        // console.log(name,value)
+        setInpval(()=>{
+            return{
+                ...inpval,[name]:value
+            }
+        })
+        
+    }
   return (
     <div className="container">
     <section className='d-flex justify-content-between'>
@@ -11,17 +31,17 @@ const Home = () => {
 
     <Form>
       <Form.Group className="mb-3 col-lg-6 " controlId="formBasicName">
-        <Form.Control type="email" placeholder="Enter Your Name" />
+        <Form.Control type="text" name='name' onChange={getData} placeholder="Enter Your Name" />
       </Form.Group>
       <Form.Group className="mb-3 col-lg-6 mt-2" controlId="formBasicEmail">
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" name='email' onChange={getData} placeholder="Enter email" />
       </Form.Group>
       <Form.Group className="mb-3 col-lg-6 mt-2" controlId="formBasicDate">
-        <Form.Control type="date"/>
+        <Form.Control type="date" name='date' onChange={getData}/>
       </Form.Group>
 
       <Form.Group className="mb-3 col-lg-6" controlId="formBasicPassword">
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" name='password' onChange={getData} placeholder="Password" />
       </Form.Group>
       <Button variant="primary" type="submit" className="col-lg-6 mt-2 mb-2">
         Submit
@@ -32,11 +52,8 @@ const Home = () => {
         Already Have an Account ? <span>SignIn</span>
     </p>
         </div>
-        <div className='right_data mt-6' style={{width:"100%"}}>
-            <div className='sign_img'>
-                <img src="https://image.shutterstock.com/image-vector/tiny-male-female-characters-huge-260nw-1982734184.jpg" alt=""/>
-            </div>
-        </div>
+        
+        <Sign_img/>
     </section>
     </div>
   )
---
+++ src/components/Sign_img.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sign_img = () => {
+  return (
+    <>
+      <div className='right_data mt-6' style={{width:"100%"}}>
+            <div className='sign_img'>
+                <img src="https://image.shutterstock.com/image-vector/tiny-male-female-characters-huge-260nw-1982734184.jpg" alt=""/>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Sign_img
